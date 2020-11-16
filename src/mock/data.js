5bf9908d0e5a39d971d9eba598ae2f7026eb795c
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-15 22:58:54 -0500

    modifying resume link

--- src/mock/data.js
+++ src/mock/data.js
@@ -12,16 +12,19 @@ export const heroData = {
   title: 'Hello, my name is',
   name: 'Niza Vera',
   subtitle: "I'm a Full Stack Developer.",
-  cta: '',
+  cta: 'Get to know me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Solution-driven and highly-collaborative Web Developer, who is ardent about integrating libraries to build incredible projects and web applications. Excelling in using technologies such as JavaScript, React, Flask, Python, SQL, and REST APIs. Through my work history, you can expect a proven ability in organization, efficiency, and teamwork.',
+  paragraphTwo:
+    'For my entire life, I have been a tech-savvy extraordinaire in various sectors of technology. When Apple started releasing the iPhone 4, I started a side hustle fixing phone screens, which eventually led to fixing hardware aspects. At my university, I was dedicated to learning about managing databases. Still, I felt that something was missing and that my intellectuality needed a full circle. Then, I was introduced to coding and so many new doors opened.',
+  paragraphThree:
+    'My passion for people and technology has inspired me to enable the full potential of humans and the internet. Continual education and keeping up with the latest technology trends are both very important to me. I am continuing to search for a future in AI and Machine Learning. Check out my Github as I regularly upload projects!',
+  resume: 'Resume', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
