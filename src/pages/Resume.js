64557a84aaf6b205d9aaae3ad95a3ac7d2ddcc64
NizaV <nizavera95@gmail.com>
2020-11-16 21:07:13 -0500

    finalized resume page with styles

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'gatsby';
 import ResumePhoto from '../images/resume.jpg';
+import '../style/resume.scss';
 
 function Resume() {
   const resumeStyles = {
@@ -7,11 +10,37 @@ function Resume() {
     height: 'auto',
   };
   return (
-    <div>
-      <div>Home</div>
-      <div>
-        <img src={ResumePhoto} alt="resume" style={resumeStyles} />
+    <div className="main">
+      <div id="resumeTop" className="header">
+        <Link to="/#about" className="ml-0">
+          <button
+            type="button"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cta-btn cta-btn--hero"
+          >
+            Take me back
+          </button>
+        </Link>
       </div>
+      <Container>
+        <div>
+          <img src={ResumePhoto} alt="resume" style={resumeStyles} />
+        </div>
+      </Container>
+
+      <footer className="footer navbar-static-bottom">
+        <Container>
+          <span className="back-to-top">
+            <Link to="#resumeTop" smooth duration={1000}>
+              <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+            </Link>
+          </span>
+
+          <hr />
+          <p className="footer__text">© {new Date().getFullYear()} NizaVera</p>
+        </Container>
+      </footer>
     </div>
   );
 }
